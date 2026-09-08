241fa94ae7d74b98e77dcb757274a178a9552493
ayushgi <gitayush30@gmail.com>
2026-09-08 09:27:55 +0530

    teacher screen

--- src/components/header.py
+++ src/components/header.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
 def header_home():
-    st.header("ATTEND-AI")
+    logo_url = "https://home.edweb.net/wp-content/uploads/snapchat.jpg"
+
+    st.markdown(f"""
+        <div style="display: flex; flex-direction:column; align-items: center; justify-content: center; margin-bottom: 20px; margin-top: 20px;">
+            <img src="{logo_url}" style="height:100px;">
+            <h1 style="text-align: center; color: white;">AttendAI</h1>
+        </div>
+    """, unsafe_allow_html=True)
